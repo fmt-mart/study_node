e634f689d0cac0c5a1caeacd8ef453f2b45386f4
tvmnhat <21t1020560@husc.edu.vn>
2025-08-08 22:35:30 +0700

    Bài học buổi 1 - NodeJS

--- src/server.js
+++ src/server.js
@@ -1,14 +1,32 @@
-import express from "express";
+/* eslint-disable no-console */
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
 
-const app = express();
+import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
-const hostname = "localhost";
-const post = 8107;
+const app = express()
 
-app.get("/", function (req, res) {
-    res.send("Hello World!");
-});
+const hostname = 'localhost'
+const port = 8017
 
-app.listen(post, hostname, () => {
-    console.log(`Server running at http://${hostname}:${post}/`);
-});
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+  console.log(mapOrder(
+    [{ id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' }],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World!</h1><hr>')
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Hello Trung Quan Dev, I am running at http://${ hostname }:${ port }/`)
+})
