f658b3d533fc85277661ed975870622d01397094
tvmnhat <21t1020560@husc.edu.vn>
2025-08-13 13:22:21 +0700

    Bai hoc buoi 4 nodejs

--- src/routes/v1/boardRoute.js
+++ src/routes/v1/boardRoute.js
@@ -4,20 +4,21 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 
+
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
-import { boardRoutes } from '~/routes/v1/boardRoute'
 
 const Router = express.Router()
-
-//Check APIs v1/status
-Router.get('/status', (req, res) => {
-  res.status(StatusCodes.OK).json({
-    message: 'APIs V1 are ready to use.'
+Router.route('/')
+  .get((req, res) => {
+    res.status(StatusCodes.OK).json({
+      message: 'Note: API get list boards'
+    })
   })
-})
+  .post((req, res) => {
+    res.status(StatusCodes.CREATED).json({
+      message: 'Note: API create new board'
+    })
+  })
 
-//Board APIs
-Router.use('/boards', boardRoutes)
-
-export const APIs_V1 = Router
+export const boardRoutes = Router
--- src/server.js
+++ src/server.js
@@ -9,14 +9,17 @@ import express from 'express'
 import exitHook from 'async-exit-hook'
 import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
+import { APIs_V1 } from '~/routes/v1'
 
 const START_SERVER = () => {
   const app = express()
 
-  app.get('/', async (req, res) => {
-    console.log(await GET_DB().listCollections().toArray())
-    res.end('<h1>Hello World!</h1><hr>')
-  })
+  app.use('/v1', APIs_V1)
+
+  // app.get('/', async (req, res) => {
+  //   console.log(await GET_DB().listCollections().toArray())
+  //   res.end('<h1>Hello World!</h1><hr>')
+  // })
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(
